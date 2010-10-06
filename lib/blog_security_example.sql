5a1ac156962c82c9b4b5f2cd43483429f001f65e
myobie <nathan@myobie.com>
2010-10-06 10:14:59 -0400

    Got rid of crap we don't need.

--- lib/blog_security_example.sql
+++ lib/blog_security_example.sql
@@ -1,11 +1,3 @@
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
-
 CREATE TABLE `comments` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `body` text NOT NULL,
@@ -14,10 +6,7 @@ CREATE TABLE `comments` (
   `email` varchar(255) NOT NULL,
   `post_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
-
-INSERT INTO `comments` VALUES(1, 'This is a comment.', 'This is a comment.', 'Nathan', 'myobie@mac.com', 1);
-INSERT INTO `comments` VALUES(2, 'M', 'M', 'N', 'E', 1);
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
 CREATE TABLE `posts` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -26,11 +15,7 @@ CREATE TABLE `posts` (
   `body_rendered` text NOT NULL,
   `user_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
-
-INSERT INTO `posts` VALUES(1, 'First post', 'First post is cool', '<p>First post is cool</p>\n', 1);
-INSERT INTO `posts` VALUES(2, 'Second poster', 'Second post is even cooler', '<p>Second post is even cooler</p>\n', 1);
-INSERT INTO `posts` VALUES(5, 'Testing everything out', 'Hello there...', '<p>Hello there...</p>\n', 0);
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -38,9 +23,8 @@ CREATE TABLE `users` (
   `email` varchar(255) NOT NULL,
   `password` varchar(40) NOT NULL,
   `salt` varchar(40) NOT NULL,
-  `token` varchar(40) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email_unique_index` (`email`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
-INSERT INTO `users` VALUES(1, 'Nathan Herald', 'myobie@me.com', 'password', 'salt', 'token');
+INSERT INTO `users` VALUES(1, 'Admin', 'admin@example.com', 'password', '--crappy salt--');
