4c752a77a8d1b99b582981f84ee0e4b37a688f09
myobie <nathan@myobie.com>
2010-10-07 08:47:06 -0400

    Protect from SQL injection from the new post form.

--- admin/create_post.php
+++ admin/create_post.php
@@ -17,12 +17,12 @@ if (! (isset($_POST["post"]) ||
 
 }
 
-$user_id = $_POST["post"]["user_id"];
-$title = $_POST["post"]["title"];
-$body = $_POST["post"]["body"];
+$user_id = $db->real_escape_string($_POST["post"]["user_id"]);
+$title = $db->real_escape_string($_POST["post"]["title"]);
+$body = $db->real_escape_string($_POST["post"]["body"]);
 $body_rendered = Markdown($body);
 
-$result = $db->query("insert into posts set user_id = '$user_id', 
+$db->query("insert into posts set user_id = '$user_id', 
   title = '$title', 
   body = '$body',
   body_rendered = '$body_rendered'");
